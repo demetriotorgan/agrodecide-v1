525ca83233c2da217b93af626d3db32e7484e2f1
demetriotorgan <demetriotorgan@hotmail.com>
2026-06-27 10:37:39 -0300

    layoutPadrao

--- src/componentes/Alerta/Alerta.jsx
+++ src/componentes/Alerta/Alerta.jsx
@@ -1,10 +1,12 @@
+import { TriangleAlert } from 'lucide-react'
 import React from 'react'
+import './Alerta.css'
 
 const Alerta = () => {
     return (
         <>
             <div className="alert-banner" role="alert">
-                <span className="alert-icon">⚠️</span>
+                <span className="alert-icon"><TriangleAlert /></span>
                 <span className="alert-text">Gerar Alertas Climáticos Importantes</span>
             </div>
         </>
--- src/componentes/Menu/Menu.jsx
+++ src/componentes/Menu/Menu.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Menu.css'
+import { ChartSpline, ClipboardClock, Cloud, FileChartPie } from 'lucide-react'
 
 const Menu = () => {
   return (
@@ -7,22 +9,22 @@ const Menu = () => {
      <section className="access-grid">
           
           <Link to="/previsao" className="access-card">
-            <div className="card-icon">☁️</div>
+            <div className="card-icon"><Cloud /></div>
             <div className="card-title">Consultar Previsões Climáticas</div>
           </Link>
           
           <Link to="/indicadores" className="access-card">
-            <div className="card-icon">📊</div>
+            <div className="card-icon"><FileChartPie /></div>
             <div className="card-title">Visualizar Indicadores Climáticos</div>
           </Link>
           
           <Link to="/historico" className="access-card">
-            <div className="card-icon">↩️</div>
+            <div className="card-icon"><ClipboardClock /></div>
             <div className="card-title">Consultar Histórico Climático</div>
           </Link>
           
           <Link to="/comparacao" className="access-card">
-            <div className="card-icon">🌐</div>
+            <div className="card-icon"><ChartSpline /></div>
             <div className="card-title">Comparar Períodos Climáticos</div>
           </Link>
         </section>
