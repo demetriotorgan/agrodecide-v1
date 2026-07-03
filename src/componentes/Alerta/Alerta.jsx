1707177551afb474c9007b342dea23b3be1b4912
demetriotorgan <demetriotorgan@hotmail.com>
2026-07-03 13:13:18 -0300

    alertaGemini

--- src/componentes/Alerta/Alerta.jsx
+++ src/componentes/Alerta/Alerta.jsx
@@ -1,39 +1,20 @@
-import { Loader2, TriangleAlert } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
-import './Alerta.css'
-import { listarModelos, obterInsightClimatico } from '../../services/apiGemini';
+import React from 'react';
+import './Alerta.css';
+import { TriangleAlert } from 'lucide-react';
 
-const Alerta = ({dadosApi}) => {
-    const [mensagemAlerta, setMensagemAlerta] = useState('Analisando dados do clima...');
-    const [carregando, setCarregando] = useState(true);
-
-//     useEffect(() => {
-//     const verificarEBuscar = async () => {
-//         console.log(import.meta.env.VITE_GEMINI_API_KEY)
-//         setCarregando(true);
-//         const sucesso = await listarModelos(); // Verifica conexão
-//         if (sucesso) {
-//             const conselho = await obterInsightClimatico(dadosApi);
-//             setMensagemAlerta(conselho);
-//         } else {
-//             setMensagemAlerta("Erro de conexão com o AgroDecide IA.");
-//         }
-//         setCarregando(false);
-//     };
-    
-//     verificarEBuscar();
-// }, [dadosApi]);
-
+const Alerta = ({ dadosApi }) => {
     return (
-        <>
-            <div className="alert-banner">
-                <div className="alert-icon">
-                    {carregando ? <Loader2 className='animate-spin'/> : <TriangleAlert />}
-                </div>
-                <p className="alert-message">{mensagemAlerta}</p>
+        <div className="alert-banner">
+            <div className="alert-icon">
+                <TriangleAlert />
             </div>
-        </>
-    )
+
+            <p className="alert-message">
+                {dadosApi?.insightAgronomico ||
+                 'Nenhuma recomendação disponível'}
+            </p>
+        </div>
+    );
 };
 
-export default Alerta
+export default Alerta;
