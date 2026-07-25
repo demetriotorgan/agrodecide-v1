f2161b6511d995b4a4ae39bfb1031bfb420c79ab
demetriotorgan <demetriotorgan@hotmail.com>
2026-07-25 04:50:33 -0300

    botaoVoltarHistorico

--- src/pages/historico/Historico.jsx
+++ src/pages/historico/Historico.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import BotaoVoltar from '../../componentes/botaoVoltar/BotaoVoltar'
+import { useNavigate } from 'react-router-dom'
 
 const Historico = () => {
+  const navigate = useNavigate();
   return (
     <>
+    <BotaoVoltar  onClick={()=> navigate('/', {repalce:true})}/>
     <p>Historico</p>
     </>
   )
